f42f6acfb3ee1f4622285cd7924349ea3664973f
eojin <eozinee@gmail.com>
2020-06-26 20:05:34 +0900

    First Commit

---
+++ Sunhwa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunhwa
+//
+//  Created by 양어진 on 2020/06/26.
+//  Copyright © 2020 eojine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
